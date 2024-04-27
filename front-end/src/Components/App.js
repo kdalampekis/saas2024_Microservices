89c83a04f54aa5f8fdf43fbd6d61cc2b66b5dce8
nikoskarakostas <el20138@mail.ntua.gr>
2024-04-27 13:12:16 +0300

    frontend files

--- front-end/src/Components/App.js
+++ front-end/src/Components/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../App.css';
+import SignUpForm from './SignUpForm';  // Import the SignUpForm component
 
 function App() {
   return (
