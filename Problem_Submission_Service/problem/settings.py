82534be61b685f2eba7321374bf7993ac30ad979
GeorgeSeretakos <g.seretakos@gmail.com>
2024-05-01 15:40:05 +0300

    all containers ok

--- Problem_Submission_Service/problem/settings.py
+++ Problem_Submission_Service/problem/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'analytics.urls'
+ROOT_URLCONF = 'problem.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'analytics.wsgi.application'
+WSGI_APPLICATION = 'problem.wsgi.application'
 
 
 # Database
