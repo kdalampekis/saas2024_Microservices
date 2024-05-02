b5af46228d3767b76b082cc42a5ba3cf5e5472d4
GeorgeSeretakos <g.seretakos@gmail.com>
2024-05-02 19:12:37 +0300

    auth-service api test page

--- Authentication_Service/authentication/settings.py
+++ Authentication_Service/authentication/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authenticationService',
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'authentication.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # Adjust if your structure is different
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Authentication_Service/authenticationService/views.py
+++ Authentication_Service/authenticationService/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from django.contrib import messages
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework import status, generics
 
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+class LoginApiView(APIView):
+    def post(self, request, *args, **kwargs):
+        print("Request data:", request.data)
+        username = request.data.get('username')
+        password = request.data.get('password')
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
-            return redirect('home')  # Redirect to a home or dashboard page
+            token, created = Token.objects.get_or_create(user=user)
+            response_data = {
+                'token': token.key,
+                'is_superuser': user.is_superuser
+            }
+            return Response(response_data)
         else:
-            messages.error(request, 'Invalid username or password')
+            return Response({"error": "Invalid credentials"}, status=400)
 
-#     return render(request, 'login.html')
-
+    def get(self, request, *args, **kwargs):
+        return render(request, "login.html")
