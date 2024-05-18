c3c05b0f259b3a6226d4ec81c2e834e3b8f3375e
GeorgeSeretakos <g.seretakos@gmail.com>
2024-05-18 20:25:24 +0300

    Problem Submission

--- Problem_Submission_Service/submission_app/admin.py
+++ Problem_Submission_Service/submission_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import SolverModel, Metadata
 
-# Register your models here.
+class SolverModelAdmin(admin.ModelAdmin):
+    list_display = ['model_id', 'title', 'notes']
+
+class MetadataAdmin(admin.ModelAdmin):
+    list_display = ['submission_id', 'username', 'date', 'credit_cost', 'problem_type']
+
+admin.site.register(SolverModel, SolverModelAdmin)
+admin.site.register(Metadata, MetadataAdmin)
